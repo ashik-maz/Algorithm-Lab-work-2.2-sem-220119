9a22ddb5b0c9ee735af403a7b2b40174e9de1c1b
ashik-maz <ashik.cse.pust@gmail.com>
2025-02-11 01:34:09 +0600

    Algorithm

---
+++ 09_fractional_knapsack.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+double maximumValue (vector<pair<int, int>>& items, int n, int w)
+{
+    sort(items.begin(),items.end(),[](pair<int,int>a,pair<int,int>b){
+        return (double)a.second/a.first>(double)b.second/b.first;
+    });
+    double maxValue=0.0;
+    for(int i=0;i<n;i++){
+        int weight=items[i].first;
+        int value=items[i].second;
+        if(w>=weight){
+            maxValue+=value;
+            w-=weight;
+        }else{
+            maxValue+=(double)w/weight*value;
+            break;
+        }
+    }
+    return maxValue;
+}
+
+int main(){
+    cout<<"Enter Number of Items: "
+    int n;cin>>n;
+    vector<pair<int,int>>items(n);
+    cout<<"Enter every item weight and value : ";
+    for(int i=0;i<n;i++){
+        cin>>items[i].first>>items[i].second;
+    }
+    cout<<"Enter Weight: ";
+    int w;cin>>w;
+    cout<<"Maximum Value="<<maximumValue(items,n,w)<<'\n';
+}
+
+
--- 12_dfs.cpp
+++ 12_dfs.cpp
@@ -1,33 +1,32 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-const int N = 1e3 + 10;
-vector<int> graph[N];
-bool vis[N];void dfs(int vertex) {
-vis[vertex] = 1;
-cout << vertex << " ";
-for (int child : graph[vertex]) {
-if (!vis[child]) {
-dfs(child);
+const int N=1e3+10;
+vector<int>graph[N];
+bool vis[N];
+void DFS(int vertex){
+    vis[vertex]=true;
+    cout<<vertex<<" ";
+    for(int child:graph[vertex]){
+        if(!vis[child]){
+            DFS(child);
+        }
+    }
 }
-}
-}
-int main() {
-int edgenumber;
-cout << "Enter the number of edges: ";
-cin >> edgenumber;
-for (int i = 0; i < edgenumber; ++i) {
-cout << "Enter source and destination vertex: ";
-int v1, v2;
-cin >> v1 >> v2;
-graph[v1].push_back(v2);
-graph[v2].push_back(v1);
-}
-int vertex;
-cout << "Enter the starting vertex for DFS: ";
-cin >> vertex;
-fill(vis, vis + N, 0);
-cout << "DFS traversal starting from node " << vertex << ": ";
-dfs(vertex);
-cout << endl;
-return 0;
+int main(){
+    cout<<"Enter the number of edge: ";
+    int edgenumber;cin>>edgenumber;
+    for(int i=0;i<edgenumber;i++){
+        cout<<"Enter source and destination : ";
+        int v1,v2;
+        cin>>v1>>v2;
+        graph[v1].push_back(v2);
+        graph[v2].push_back(v1);
+    }
+    int vertex;
+    cout<<"Enter the staring vertex for DFS: ";
+    cin>>vertex;
+    cout<<"DFS: ";
+    DFS(vertex);
+    cout<<'\n';
+    return 0;
 }
