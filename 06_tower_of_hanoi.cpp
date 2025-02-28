0d1720cf15c75ec02b6df4a944def8df4e1c2908
ashik-maz <ashik.cse.pust@gmail.com>
2025-02-28 23:44:18 +0600

    Algorithm

--- 06_tower_of_hanoi.cpp
+++ 06_tower_of_hanoi.cpp
@@ -5,9 +5,7 @@ void TowerOfHanoi(int n, char src, char aux, char dest){
     if(n==0) return;
 
     TowerOfHanoi(n-1,src,dest,aux);
-
     cout<<"Move disk "<<n<<" from "<<src<<" to "<<dest<<endl;
-    
     TowerOfHanoi(n-1,aux,src,dest);
 }
 
